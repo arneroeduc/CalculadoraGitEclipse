01a0f3ccedfe95e2f3c85459ef79eedf50e0d182
arner0 <arner0@localhost>
2025-02-21 14:29:16 +0100

    v2

--- Calculadora/src/Calculadora/Calculadora.java
+++ Calculadora/src/Calculadora/Calculadora.java
@@ -1,6 +1,8 @@
 package Calculadora;
 
 public class Calculadora {
+	
+
 	public int sumar(int a, int b) {
 		return a + b;
 	}
@@ -15,6 +17,6 @@ public class Calculadora {
 
 	public static void main(String[] args) {
 		Calculadora calc = new Calculadora();
-		System.out.println("Resultado de la suma: " + calc.sumar(5, 3));
+		System.out.println("Resultado de la suma: " + calc.sumar(5, 4));
 	}
 }
